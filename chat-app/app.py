d7872a09facb85d8188f944d31b64a19f287355e
Muhammad Usama <51862131+Muhammad-Usama-07@users.noreply.github.com>
2023-06-07 17:18:55 +0500

    adding rules for chatbot reply

--- chat-app/app.py
+++ chat-app/app.py
@@ -6,6 +6,23 @@ app = Flask(__name__)
 # app.config['SECRET_KEY'] = 'your-secret-key'
 socketio = SocketIO(app)
 
+##### chatbot reply start
+rules = {'I want (.*)': ['What would it mean if you got {0}',
+  'Why do you want {0}',
+  "What's stopping you from getting {0}"],
+ 'do you remember (.*)': ['Did you think I would forget {0}',
+  "Why haven't you been able to forget {0}",
+  'What about {0}',
+  'Yes .. and?'],
+ 'do you think (.*)': ['if {0}? Absolutely.', 'No chance'],
+ 'if (.*)': ["Do you really think it's likely that {0}",
+  'Do you wish that {0}',
+  'What do you think about {0}',
+  'Really--if {0}']}
+
+##### chatbot reply start
+
+
 # Create a route for the home page:
 @app.route('/')
 def index():
